6c522a2101c1ce3ed842fd389b0e34a651c86dff
Thezuckq <dzakkyhazimy@gmail.com>
2021-08-03 23:57:42 +0700

    add-css

--- gigih-app/src/App.js
+++ gigih-app/src/App.js
@@ -3,6 +3,7 @@ import MyRouter from "./config/routes";
 
 
 function App() {
+  // eslint-disable-next-line no-undef
   console.log(process.env.REACT_APP_YOUR_GIGIH_APP_API_KEY);
 
   return (
--- gigih-app/src/pages/login/index.js
+++ gigih-app/src/pages/login/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import axios from "axios";
-import { Button } from "../../components";
+import { ButtonCompo } from "../../components";
 import { API_SCOPE, REDIRECT_URI } from "../../config/constant";
 function Login() {
 
   const linkURL = `https://accounts.spotify.com/en/authorize?client_id=${
+    // eslint-disable-next-line no-undef
     process.env.REACT_APP_YOUR_GIGIH_APP_API_KEY
   }&response_type=${"token"}&redirect_uri=${REDIRECT_URI}&scope=${API_SCOPE}`;
 
   return (
     <div className="SignIn">
-      <Button name="Login" link={linkURL} />
+      <ButtonCompo name="Login" link={linkURL} />
     </div>
   );
 }
