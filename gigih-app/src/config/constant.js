56aabc8fac3e0d780f967ca9dfb831779d28c53c
Thezuckq <dzakkyhazimy@gmail.com>
2021-08-14 20:32:48 +0700

    test-deploy-second

--- gigih-app/src/config/constant.js
+++ gigih-app/src/config/constant.js
@@ -1,6 +1,6 @@
 export const SPOTIFY_ENDPOINT = "https://api.spotify.com/v1";
 export const API_SCOPE = "playlist-modify-private";
-export const REDIRECT_URI = "https://generasi-gigih-homework-hk0ib4e9f-thezuckq.vercel.app/";
+export const REDIRECT_URI = "https://generasi-gigih-homework-delta.vercel.app/";
 export const SIGNIN_URL = `https://accounts.spotify.com/en/authorize?client_id=${
   // eslint-disable-next-line no-undef
   process.env.REACT_APP_YOUR_GIGIH_APP_API_KEY
